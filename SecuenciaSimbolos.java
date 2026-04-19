d32e33d709ed1e7f503bcd474afbb4848035b1f7
cardenas17 <mateo.cardenas03@epn.edu.ec>
2026-04-19 11:39:20 -0500

    Creacion de feacture-SecuenciaSimbolos

---
+++ SecuenciaSimbolos.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SecuenciaSimbolos {
+    public void generarSecuencia() {
+        // Creamos el scanner para leer la entrada del usuario
+        Scanner lectura = new Scanner(System.in);
+        
+        System.out.print("Introduce la longitud de la secuencia: ");
+        
+        // Validamos que sea un número entero
+        if (lectura.hasNextInt()) {
+            int n = lectura.nextInt();
+            
+            System.out.println("Tu secuencia es:");
+            
+            // Ciclo para generar la secuencia
+            for (int i = 0; i < n; i++) {
+                if (i % 2 == 0) {
+                    System.out.print("+");
+                } else {
+                    System.out.print("-");
+                }
+            }
+            // Salto de línea al final
+            System.out.println(); 
+            
+        } else {
+            System.out.println("Error: Debes ingresar un número entero válido.");
+        }
+        
+        lectura.close();
+    }
+}
